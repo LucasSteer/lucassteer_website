604ea41dc2abd50ca81d485f442bc8add7cc2eb6
Lucas Steer <lucassteer@gmail.com>
2022-05-05 23:14:59 -0400

    Convert myprojects to place cards based on data

--- pages/myprojects.vue
+++ pages/myprojects.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="flex flex-row flex-wrap justify-around">
     <ProjectCard
-      title="GeocARching"
-      subtitle="Geocaching implemented in AR for Android devices"
-      link="https://github.com/LucasSteer/CIS4250_GeocARching"
-      img-src="/thumbnails/GeocARching.jpg"
-      img-alt="A black-and-white photo of a 3D model of a cactus in AR"
+      v-for="project in projects"
+      :key="project.title"
+      :title="project.title"
+      :subtitle="project.subtitle"
+      :link="project.link"
+      :imgSrc="project.imgSrc"
+      :imgAlt="project.imgAlt"
     />
   </main>
 </template>
@@ -21,6 +23,19 @@ export default {
   head() {
     return {
       title: 'My Projects | Lucas Steer',
+    };
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: 'GeocARching',
+          subtitle: 'Geocaching implemented in AR for Android devices',
+          link: 'https://github.com/LucasSteer/CIS4250_GeocARching',
+          imgSrc: '/thumbnails/GeocARching.jpg',
+          imgAlt: 'A black-and-white photo of a 3D model of a cactus in AR',
+        },
+      ],
     };
   },
 };
