0bf521c988be8c0b8f86adab04d64006dfd6c413
Lucas Steer <lucassteer@gmail.com>
2023-12-20 00:37:14 -0500

    Convert project card to have just the heading be a link - improve markup semantics - add click handler for the entire section to maintain existing click area - note: this link doesn't need to open in a new tab per https://www.w3.org/TR/WCAG20-TECHS/G200.html - note: there is no "active-within" equivalent to "focus-within" for active link - note: modifierKey+Click behaviour only works when clicking directly on the link -- comparable to this example: https://inclusive-components.design/cards/

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,35 +1,34 @@
 <template>
   <section
-    class="relative overflow-hidden rounded-lg shadow-card forced-colors:border-4 forced-colors:border-systemColors-ButtonBorder dark:shadow-none dark:border-4 dark:border-grey-400"
+    class="group relative cursor-pointer overflow-hidden rounded-lg shadow-card forced-colors:border-4 forced-colors:border-systemColors-ButtonBorder dark:shadow-none dark:border-4 dark:border-grey-400"
+    @click="openLink($event, link)"
   >
-    <InlineLink
-      :url="link"
-      class="group text-grey no-underline dark:text-white hover:text-grey dark:hover:text-white focus:text-grey dark:focus:text-white"
+    <img :src="imgSrc" :alt="imgAlt" />
+    <div
+      class="min-h-1/6 w-full tablet:absolute tablet:bottom-0 bg-white dark:bg-zinc-800 forced-colors:group-hover:text-systemColors-SelectedItemText forced-colors:focus-within:text-systemColors-SelectedItemText forced-colors:group-active:text-systemColors-SelectedItemText forced-colors:group-hover:bg-systemColors-SelectedItem forced-colors:focus-within:bg-systemColors-SelectedItem forced-colors:group-active:bg-systemColors-SelectedItem group-hover:bg-grey-50 focus-within:bg-grey-50 group-active:bg-grey-100 dark:group-hover:bg-zinc-700 dark:focus-within:bg-zinc-700 dark:group-active:bg-zinc-600 px-4 pt-2 pb-8 tablet:px-8 tablet:pt-3 tablet:pb-10"
     >
-      <img :src="imgSrc" :alt="imgAlt" class="tablet:h-full w-full" />
-      <div
-        class="min-h-1/6 w-full tablet:absolute tablet:bottom-0 flex flex-col justify-center gap-2 bg-white dark:bg-zinc-800 forced-colors:group-hover:text-systemColors-SelectedItemText forced-colors:group-focus:text-systemColors-SelectedItemText forced-colors:group-active:text-systemColors-SelectedItemText forced-colors:group-hover:bg-systemColors-SelectedItem forced-colors:group-focus:bg-systemColors-SelectedItem forced-colors:group-active:bg-systemColors-SelectedItem group-hover:bg-grey-50 group-focus:bg-grey-50 group-active:bg-grey-100 dark:group-hover:bg-zinc-700 dark:group-focus:bg-zinc-700 dark:group-active:bg-zinc-600 px-4 pt-2 pb-8 tablet:px-8 tablet:pt-3 tablet:pb-10"
-      >
+      <InlineLink :url="link" class="group/link no-underline">
         <h2
-          class="text-xl decoration-2 underline-offset-4 underline group-hover:decoration-4 group-focus:decoration-4 tracking-widest text-green-baseLarge dark:text-greenDarkMode-baseLarge tablet:text-2xl desktop:text-4xl"
+          class="inline text-xl underline decoration-2 underline-offset-4 group-hover:decoration-4 group-focus/link:decoration-4 tracking-widest text-blue-baseLarge dark:text-blueDarkMode-baseLarge tablet:text-3xl desktop:text-4xl"
         >
           {{ title }}
         </h2>
-        <h3
-          class="text-sm tracking-wider tablet:text-base tablet:leading-8 desktop:text-xl"
+      </InlineLink>
+      <p
+        class="text-sm tracking-wider my-2 tablet:text-base tablet:leading-8 desktop:text-xl"
+      >
+        {{ subtitle }}
+      </p>
+      <ul class="mt-4 flex flex-row flex-wrap gap-4">
+        <li
+          v-for="(tag, index) in tags"
+          :key="index"
+          class="rounded-md bg-primary-base forced-colors:border-2 forced-colors:border-systemColors-ButtonBorder dark:bg-primaryDarkMode-base font-normal forced-colors:group-focus:text-systemColors-SelectedItemText forced-colors:group-focus:bg-systemColors-SelectedItem dark:font-bold tracking-wide p-2 align-middle leading-4 text-white dark:text-grey tablet:text-lg tablet:leading-4 desktop:text-xl"
         >
-          {{ subtitle }}
-        </h3>
-        <div class="mt-4 flex flex-row flex-wrap gap-4">
-          <span
-            v-for="(tag, index) in tags"
-            :key="index"
-            class="rounded-md bg-primary-base forced-colors:border-2 forced-colors:border-systemColors-ButtonBorder dark:bg-primaryDarkMode-base font-normal forced-colors:group-hover:text-systemColors-SelectedItemText forced-colors:group-focus:text-systemColors-SelectedItemText forced-colors:group-active:text-systemColors-SelectedItemText forced-colors:group-hover:bg-systemColors-SelectedItem forced-colors:group-focus:bg-systemColors-SelectedItem forced-colors:group-active:bg-systemColors-SelectedItem dark:font-bold tracking-wide p-2 align-middle leading-4 text-white dark:text-grey tablet:text-lg tablet:leading-4 desktop:text-xl"
-            >{{ tag }}</span
-          >
-        </div>
-      </div>
-    </InlineLink>
+          {{ tag }}
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -62,6 +61,12 @@ export default {
       default: () => {
         [];
       },
+    },
+  },
+  methods: {
+    openLink(event, url) {
+      location.href = url;
+      event.preventDefault();
     },
   },
 };
